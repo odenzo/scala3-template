3765fb8ee7c9d2be5d339a93f477bc431d404029
Scala Steward <me@scala-steward.org>
2021-09-18 18:35:06 +0200

    Update slf4s to 0.2.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -22,7 +22,7 @@ object Dependencies {
     val scalaTags  = "0.9.4"
     val scodec     = "2.0.0"
     val scodecBits = "1.1.27"
-    val slf4s      = "0.2.0" // This should bring in slf4j
+    val slf4s      = "0.2.1" // This should bring in slf4j
     //val squantsV                  = "1.7.4"
   }
 
