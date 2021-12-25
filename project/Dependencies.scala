b7299c5b8055bb7a339885774c44e88bfedd5a29
Scala Steward <me@scala-steward.org>
2021-12-26 00:46:59 +0100

    Update fs2-core, fs2-io, ... to 3.2.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
     val catsEffect = "3.3.0"
     val circe      = "0.14.1"
     val doobie     = "1.0.0-RC1"
-    val fs2        = "3.2.3"
+    val fs2        = "3.2.4"
 
     val logback = "1.2.7"
 
