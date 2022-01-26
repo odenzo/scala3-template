0ce0eea9e30f9ebb261811ba1c48cd1c2a72f7a2
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-01-26 14:05:10 +0100

    Update doobie-core, doobie-hikari, ... to 1.0.0-RC2 (#100)
    Co-authored-by: odenzo <3457680+odenzo@users.noreply.github.com>

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,6 +4,7 @@ object Dependencies {
     val cats       = "2.7.0"
     val catsEffect = "3.3.4"
     val circe      = "0.14.1"
+    val doobie     = "1.0.0-RC2"
     val doobie     = "1.0.0-RC1"
     val fs2        = "3.2.4"
 
