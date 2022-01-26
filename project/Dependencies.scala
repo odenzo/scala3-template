a2c22d083706542662b68c080fb8ca02fdbc5851
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-01-26 14:03:16 +0100

    Update cats-effect to 3.3.4 (#99)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 object Dependencies {
   object Version {
     val cats       = "2.7.0"
-    val catsEffect = "3.3.0"
+    val catsEffect = "3.3.4"
     val circe      = "0.14.1"
     val doobie     = "1.0.0-RC1"
     val fs2        = "3.2.4"
