dec5235ff903cf3db5822d12eb0c286d0b8deb59
stevef <pania.misc@gmail.com>
2022-02-04 06:31:56 +1300

    Dependencies update fix

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,32 +1,5 @@
 import sbt._
 object Dependencies {
-  object Version {
-    val cats       = "2.7.0"
-    val catsEffect = "3.3.4"
-    val circe      = "0.14.1"
-    val doobie     = "1.0.0-RC2"
-    // val doobie     = "1.0.0-RC1"
-    val fs2        = "3.2.4"
-
-    val logback = "1.2.10"
-
-    val http4s    = "0.23.8"
-    val mockito   = "3.10.0"
-    val monocle   = "3.1.0"
-    val munit     = "0.7.29"
-    val munitCats = "1.0.7"
-
-    val osLib      = "0.8.0"
-    val pprint     = "0.7.1"
-    val scala      = "1.15.3"
-    val scalaCss   = "0.7.0"
-    val scalaTags  = "0.9.4"
-    val scodec     = "2.1.0"
-    val scodecBits = "1.1.30"
-    val slf4s      = "0.3.0" // This should bring in slf4j
-    // val squantsV                  = "1.7.4"
-
-  }
 
   lazy val stdLibs = Seq(
     "com.lihaoyi"          %% "pprint"          % Version.pprint,
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,2 +1,3 @@
 //addSbtPlugin("com.typesafe.sbt" % "sbt-native-packager" % "1.8.0")
-addSbtPlugin("com.timushev.sbt" % "sbt-updates" % "0.6.1") // In Global
+addSbtPlugin("com.timushev.sbt" % "sbt-updates"   % "0.6.1") // In Global
+addSbtPlugin("com.eed3si9n"     % "sbt-buildinfo" % "0.10.0")
