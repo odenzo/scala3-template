5965e1512237eeb38a23843d0f9ddc9cb6ee80c5
Scala Steward <scala_steward@virtuslab.com>
2022-09-17 20:22:38 +0000

    Update http4s-circe, http4s-core, ... to 0.23.16

--- project/Version.scala
+++ project/Version.scala
@@ -1,17 +1,17 @@
 import sbt._
 
 object Version {
-  val cats       = "2.7.0"
+  val cats = "2.7.0"
 
   val catsEffect = "3.3.12"
   val circe      = "0.14.1"
 
-  val doobie     = "1.0.0-RC2"
-  val fs2        = "3.2.7"
+  val doobie = "1.0.0-RC2"
+  val fs2    = "3.2.7"
 
   val logback = "1.2.11"
 
-  val http4s    = "0.23.12"
+  val http4s    = "0.23.16"
   val mockito   = "3.10.0"
   val monocle   = "3.1.0"
   val munit     = "0.7.29"
