956508ba73c2d13a94dcd01385d33e8481627fd5
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2022-11-03 17:17:48 +0100

    Update scalafmt-core to 3.5.9 (#147)
    * Update scalafmt-core to 3.5.9
    
    * Reformat with scalafmt 3.5.9
    
    Executed command: scalafmt --non-interactive
    
    * Add 'Reformat with scalafmt 3.5.9' to .git-blame-ignore-revs
    
    * Revert commit(s) 10ba8bf, f19544c, f8a59b3
    
    * Update scalafmt-core to 3.5.9
    
    * Reformat with scalafmt 3.5.9
    
    Executed command: scalafmt --non-interactive
    
    * Add 'Reformat with scalafmt 3.5.9' to .git-blame-ignore-revs

--- app/yangSchemaParser/src/main/scala/com/odenzo/yang/schema/Blah.scala
+++ app/yangSchemaParser/src/main/scala/com/odenzo/yang/schema/Blah.scala
@@ -2,6 +2,6 @@ package com.odenzo.yang.schema
 
 object Blah {
 
-  val stream = fs2.Stream(1,3,4,5,5,6,7)
-  stream.changesBy((v: Int) => v %2) // Ignore consecutive even or odd
+  val stream = fs2.Stream(1, 3, 4, 5, 5, 6, 7)
+  stream.changesBy((v: Int) => v % 2) // Ignore consecutive even or odd
 }
